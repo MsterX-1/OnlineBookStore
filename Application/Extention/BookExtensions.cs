9c9d5ac849468715b0c18bdb4647071c6da3f729
Youssef Ibrahim <es-youssef.taawash2027@alexu.edu.eg>
2025-12-19 21:39:12 +0200

    ReConstruct the Book

--- Application/Extention/BookExtensions.cs
+++ Application/Extention/BookExtensions.cs
@@ -10,37 +10,6 @@ namespace Application.Extention
 {
     public static class BookExtensions
     {
-        public static GetBookDto ConvertToGetBookDto(this Book book, string? publisherName = null, List<string>? authors = null)
-        {
-            return new GetBookDto
-            {
-                ISBN = book.ISBN,
-                Title = book.Title,
-                PubYear = book.Pub_Year,
-                Price = book.Price,
-                Category = book.Category,
-                StockQty = book.Stock_Qty,
-                Threshold = book.Threshold,
-                PublisherId = book.Publisher_ID,
-                PublisherName = publisherName,
-                Authors = authors
-            };
-        }
-
-        public static Book ConvertToBook(this CreateBookDto dto)
-        {
-            return new Book
-            {
-                ISBN = dto.ISBN,
-                Title = dto.Title,
-                Pub_Year = dto.PubYear,
-                Price = dto.Price,
-                Category = dto.Category,
-                Stock_Qty = dto.StockQty,
-                Threshold = dto.Threshold,
-                Publisher_ID = dto.PublisherId
-            };
-        }
-
+        
     }
 }
--- Application/Interfaces/IBookRepository.cs
+++ Application/Interfaces/IBookRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Dtos.BookDto;
 using Domain.Models;
 
 namespace Application.Interfaces
@@ -10,18 +11,13 @@ namespace Application.Interfaces
     public interface IBookRepository
     {
         // Define method signatures for book repository operations
-        Task<IEnumerable<Book>> GetAllBooksAsync();
-        Task<Book?> GetBookByISBNAsync(string isbn);
-        Task<IEnumerable<Book>> GetBooksByCategoryAsync(string category);
-        Task<IEnumerable<Book>> SearchBooksByTitleAsync(string title);
-        Task<IEnumerable<Book>> GetLowStockBooksAsync();
-        Task<bool> CreateBookAsync(Book book);
-        Task<bool> UpdateBookAsync(Book book);
-        Task<bool> UpdateBookStockAsync(string isbn, int newStock);
-        Task<bool> DeleteBookAsync(string isbn);
-        Task<IEnumerable<Author>> GetBookAuthorsAsync(string isbn);
-        Task<bool> AddBookAuthorAsync(string isbn, int authorId);
-        Task<bool> RemoveBookAuthorAsync(string isbn, int authorId);
-        Task<string?> GetPublisherNameAsync(int publisherId);
+        Task<IEnumerable<GetBookDto>> GetAllBooksAsync();
+        //Task<Book?> GetBookByISBNAsync(string isbn);
+        //Task<IEnumerable<Book>> GetBooksByCategoryAsync(string category);
+        //Task<IEnumerable<Book>> SearchBooksByTitleAsync(string title);
+        //Task<IEnumerable<Book>> GetLowStockBooksAsync();
+        //Task<bool> CreateBookAsync(Book book);
+        //Task<bool> UpdateBookAsync(Book book);
+        //Task<bool> UpdateBookStockAsync(string isbn, int newStock);
     }
 }
